0a7dd9cd03c4d8da33eb6509fa0594ed660fc18d
MIRZA AHMER BAIG <mahmerbaig3@gmail.com>
2022-05-25 01:21:35 +0500

    chapter 1 and 2
    JAVA SCRIPT ASSINMENT OF CHAPTER 1 AND 2

---
+++ app.js
@@ -0,0 +1,49 @@
+// chapter 1
+// question 1
+alert("CHAPTER 1")
+alert("WELCOME TO MY WEBSITE")
+// QUESTION 2
+alert("Error! Please Enter a valid password")
+// QUESTION 3
+alert("WELCOME TO JS LAND\nHAPPY CODING")
+// QUESTION 4
+alert("WELCOME TO JS LAND....")
+alert("HAPPY CODING!")
+// QUESTION 5
+alert("Hello.. I can run JS through my web browser's console")
+
+// CHAPTER 2
+alert("CHAPTER 2")
+// question 1
+var username="MIRZA";
+alert(username)
+var myname="MIRZA AHMER BAIG";
+alert(myname);
+// QUESTION 2
+var message="HELLO WORLD"
+alert(message);
+// QUESTION NO 3
+var studentName="MIRZA AHMER BAIG";
+alert(studentName);
+var studentAge="15 years old";
+alert(studentAge);
+var qualification="CERTIFIED MOBILE APPLICATION DEVELOPMENT";
+alert(qualification);
+// QUESTION NO 4
+var dish="PIZZA\nPIZZ\nPIZ\nPI\nP"
+alert(dish);
+// QUESTION NO 5
+var email="MY EMAIL ADDRESS IS example@example.com"
+alert(email);
+// QUESTION NO 6
+var book="A SMARTER WAY TO LEARN JAVASCRIPT"
+alert("I AM TRYING TO LEARN FROM THE BOOK " + book);
+// question no 7
+document.write("Yah! I can write HTML content through JavaScript");
+// Question 8
+
+var pattern = "▬▬▬▬▬▬▬▬▬ஜ ۩۞۩ ஜ▬▬▬▬▬▬▬▬▬" ;
+alert(pattern);
+
+
+
